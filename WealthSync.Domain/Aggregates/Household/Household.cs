ec2c96ce7238f1fe54dba7f00b3c62a0423d3f47
nelsonFAlmeida <a22304590@alunos.ulht.pt>
2026-07-12 21:19:15 +0100

    create Household entity and initial xUnit tests

---
+++ WealthSync.Domain/Aggregates/Household/Household.cs
@@ -0,0 +1,26 @@
+﻿namespace WealthSync.Domain.Aggregates.Household;
+using System;
+
+
+public class Household
+{
+    public Guid Id { get; private set; }
+    public String Name { get; private set; }
+    
+    public Household(String name)
+    {
+        if (String.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Invalid Household Name");
+        }
+
+        if (name.Length < 3)
+        {
+            throw new ArgumentException("Household needs 3 letters at least");
+        }
+
+        Id = Guid.NewGuid();
+        this.Name = name;
+    }
+
+}
---
+++ WealthSync.Tests/Domain/Aggregates/Household/HouseholdTests.cs
@@ -0,0 +1,33 @@
+﻿namespace WealthSync.Tests.Domain.Aggregates.Household;
+
+using System;
+using Xunit;
+using WealthSync.Domain.Aggregates.Household;
+
+public class HouseholdTests
+{
+    [Fact]
+    public void HouseholdValidCreateTest()
+    {
+        var householdName = "Nelson's Household";
+        
+        var household = new Household(householdName);
+        
+        Assert.NotNull(household);
+        Assert.Equal(householdName, household.Name);
+        
+        Assert.NotEqual(Guid.Empty, household.Id);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void HouseholdInvalidNameTest(String invalidName)
+    {
+        var exceptionMensage = Assert.Throws<ArgumentException>(() =>
+            new Household(invalidName));
+        
+        Assert.Equal("Invalid Household Name", exceptionMensage.Message);
+    }
+}
